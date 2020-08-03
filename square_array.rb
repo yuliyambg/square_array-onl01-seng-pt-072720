3e180ca9b16a48da1badfd9e4b87ff4512269b02
secure-revert-8990 <yuliyambg@gmail.com>
2020-08-03 19:36:17 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,17 @@
-def square_array(numbers)
-  # your code here
-    new_array[]
-    numbers.collect do |num|
-    new_array << (num**2)
-   end
-   return new_array
-end
+# def square_array(numbers)
+#   # your code here
+#     new_array[]
+#     numbers.collect do |num|
+#     new_array << (num**2)
+#   end
+#   return new_array
+# end
 
+def square_array(numbers)
+  # your code here	  
+  new_numbers = []
+end 	  numbers.each do |num|
+    new_numbers << (num ** 2)
+  end
+  return new_numbers
+end
